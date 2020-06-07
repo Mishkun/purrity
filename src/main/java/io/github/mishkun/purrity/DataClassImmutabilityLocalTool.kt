7ca3e93c0f6da0bc67acafc7043832c572498ba5
Mikhail Levchenko <mishkun.lev@gmail.com>
2020-06-07 22:59:43 +0300

    Add check for enums and rename tool

--- src/main/java/io/github/mishkun/purrity/DataClassImmutabilityLocalTool.kt
+++ src/main/java/io/github/mishkun/purrity/DataClassImmutabilityLocalTool.kt
@@ -3,6 +3,7 @@ package io.github.mishkun.purrity
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.canBeUsedForConstVal
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.lexer.KtTokens.VAR_KEYWORD
@@ -12,12 +13,12 @@ import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 
-class CheckImmutableDataClassLocal : LocalInspectionTool() {
+class DataClassImmutabilityLocalTool : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : KtTreeVisitorVoid() {
             override fun visitClass(ktClass: KtClass) {
                 super.visitClass(ktClass)
-                if (!ktClass.isData()) return
+                if (!ktClass.isData() && !ktClass.isEnum()) return
                 if (ktClass.annotationEntries.any { it.text.toString() == "@Mutable" }) return
                 val constructor = ktClass.primaryConstructor ?: return
                 val context = constructor.analyze(BodyResolveMode.FULL)
@@ -32,7 +33,7 @@ class CheckImmutableDataClassLocal : LocalInspectionTool() {
                     val typeDescriptor = context.get(BindingContext.TYPE, typeReference) ?: continue
                     val classDescriptor = DescriptorUtils.getClassDescriptorForType(typeDescriptor)
                     val fqName = classDescriptor.let { DescriptorUtils.getFqName(it) }
-                    if (!(typeDescriptor.canBeUsedForConstVal() || classDescriptor.isData)) {
+                    if (!(typeDescriptor.canBeUsedForConstVal() || classDescriptor.isData || classDescriptor.kind == ClassKind.ENUM_CLASS)) {
                         if (classDescriptor.annotations.all { "EffectivelyImmutable" !in it.fqName.toString()}) {
                             holder.registerProblem(typeReference, "Data class parameter is not data class itself, but $fqName!")
                         }
